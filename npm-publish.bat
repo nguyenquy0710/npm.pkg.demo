718678ffbfba868c684d55c83a105792b4516828
Nguyễn Hữu Quý <22485887+nguyenquy0710@users.noreply.github.com>
2024-01-02 12:28:25 +0000

    up code

---
+++ npm-publish.bat
@@ -0,0 +1,6 @@
+@REM Publishing a package with a dist-tag
+@REM https://docs.npmjs.com/adding-dist-tags-to-packages#publishing-a-package-with-a-dist-tag
+
+@REM npm publish --tag <tag>
+
+npm publish --tag beta
